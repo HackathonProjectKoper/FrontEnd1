dde7f2eed0e566c3ba2461ff469c62af51cc043f
David <zlatanoskidavid92@gmail.com>
2025-05-12 16:38:12 +0200

    First version of the login form

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {LoginForm} from "@/components/login-form.jsx";
+
+
+const App = () => {
+    return (
+
+        <LoginForm className="flex flex-col gap-6 justify-center items-center min-h-screen "/>
+    )
+}
+export default App
